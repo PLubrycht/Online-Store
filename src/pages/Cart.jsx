579c6c33971592b88b9ecededd524911f0da2465
Pawel Lubrycht <plubrycht@gmail.com>
2025-06-24 20:56:32 +0200

    creating working cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,28 @@
-function Cart () {
-    return <h1>Koszyk</h1>
-}
+import { useCart } from "../context/CartContext";
+
+const Cart = () => {
+  const { cart, removeFromCart, clearCart } = useCart();
+
+  return (
+    <div>
+      <h2>Twój koszyk</h2>
+      {cart.length === 0 ? (
+        <p>Koszyk jest pusty</p>
+      ) : (
+        <>
+          <ul>
+            {cart.map((item, index) => (
+              <li key={index}>
+                {item.name} - {item.price} zł
+                <button onClick={() => removeFromCart(item.id)}>Usuń</button>
+              </li>
+            ))}
+          </ul>
+          <button onClick={clearCart}>Wyczyść koszyk</button>
+        </>
+      )}
+    </div>
+  );
+};
+
 export default Cart;
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,9 +1,11 @@
 import { useParams } from 'react-router-dom';
 import mockProducts from '../api/mockProducts';
 import './ProductDetails.css';
+import { useCart } from "../context/CartContext";
 
 const ProductDetails = () => {
   const { id } = useParams();
+  const { addToCart } = useCart();
   const product = mockProducts.find((item) => item.id === parseInt(id));
   if (!product) return <h2>Produkt nie znaleziony</h2>;
 
@@ -13,6 +15,7 @@ const ProductDetails = () => {
       <img src={product.image} alt={product.name} />
       <p>Cena: {product.price} PLN</p>
       <p>{product.description}</p>
+      <button onClick={() => addToCart(product)}>Dodaj do koszyka</button>
     </div>
   );
 };
